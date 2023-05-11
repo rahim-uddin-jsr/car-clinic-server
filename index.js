98d09bda34949632d5541105d74484fbbbe36af9
dev-rahim <rahimuddin5915@gmail.com>
2023-05-11 11:02:45 +0600

    created a get api for get data from database

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("car-clinic");
+    const servicesCollection = database.collection("servicesCollection");
+
+    app.get("/services", async (req, res) => {
+      const data = servicesCollection.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
